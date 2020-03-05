aa1b7f46c6b8c1f71beb424dbd6099d4309141ee
alexlastin <61705879+alexlastin@users.noreply.github.com>
2020-03-05 18:57:47 +0300

    Delete join_script.sql

--- join_script.sql
+++
@@ -1,5 +0,0 @@
-
-
-SELECT TotalQuantity, Product.Name FROM
-(SELECT ProductID, SUM(Quantity) as TotalQuantity FROM Sales GROUP BY ProductID) AS AggregateSales
-LEFT JOIN Product on AggregateSales.ProductID = Product.id
